87b294c7227a9fde1df8a4509263067ceec490dc
Smith <chendingchao1@126.com>
2017-06-03 18:50:39 +0800

    添加ehcache

---
+++ src/test/java/app/app/gentle/cache/Test.java
@@ -0,0 +1,34 @@
+package app.app.gentle.cache;
+
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+
+/**
+ * Created by Smith on 2017/6/3.
+ */
+public class Test {
+    public static void main(String[] args) {
+        CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+                .withCache("preConfigured",
+                        CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class, ResourcePoolsBuilder.heap(10)))
+                .build();
+        cacheManager.init();
+
+        Cache<Long, String> preConfigured =
+                cacheManager.getCache("preConfigured", Long.class, String.class);
+
+        Cache<Long, String> myCache = cacheManager.createCache("myCache",
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class, ResourcePoolsBuilder.heap(10)));
+
+        preConfigured.put(1L, "helloworld");
+        myCache.put(1L, "da one!");
+        String value = myCache.get(1L);
+        System.out.println(value);
+        System.out.println(preConfigured.get(1L));
+
+        cacheManager.close();
+    }
+}
